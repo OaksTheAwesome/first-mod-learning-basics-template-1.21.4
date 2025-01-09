d976c8639fbcfdba3575bca9494cf5a4fbb27322
OaksTheAwesome <oakstheawesome@gmail.com>
2025-01-09 16:54:22 -0600

    Initial commit

---
+++ src/main/java/net/oakstheawesome/firstmodlearningbasics/FirstModLearningBasicsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.oakstheawesome.firstmodlearningbasics;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FirstModLearningBasicsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
